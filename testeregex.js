463a4d3669010ae698f8c9ce10ef14c6b0fc543e
Ariane Barros <arianepaulabarros@gmail.com>
2021-09-30 12:21:26 -0300

    Generating random licenses

--- testeregex.js
+++ testeregex.js
@@ -5,15 +5,26 @@ var capital_letters_array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "
 var myArray = [];
 var cont = 0;
 while(cont < 20){
-    var valor_gerado = Math.floor(Math.random() * 26)
-    myArray.push(valor_gerado)
+    var placa = "";
+    
+    for (i = 0; i < 3; i++){
+        var valor_gerado = Math.floor(Math.random() * 26)
+        placa += capital_letters_array[valor_gerado];
+    }
+
+    for (i = 0; i < 4; i++) {
+        var valor_gerado = Math.floor(Math.random() * 9)
+        placa += valor_gerado.toString();
+    }
+    
+    myArray.push(placa)
     cont++;
 }
-
+console.log(myArray);
 //var myArray = ['AAA8A88', 'A909090', 'aaa8a88', 'aAA8A88', '12AAAAA', 'AAA9A99AA', 'AAA8888'];
 var validated = []
 myArray.forEach(function(item){
-    var value = item.match(re);
+    var value = item.toString().match(re);
 
     if(value != null)
         validated.push(value);

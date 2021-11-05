d5159baa6a386e9c9b35b54505db91871f920e5c
Ariane Barros <arianepaulabarros@gmail.com>
2021-11-05 10:13:41 -0300

    BD connection working

--- bd.js
+++ bd.js
@@ -1,40 +1,92 @@
-
-const http = require('http');
+const express = require('express')
 const oracledb = require('oracledb');
-let error;
-let user;
- //erro: ORA-12154: TNS: could not resolve the connect identifier specified
-oracledb.getConnection(
-    {
-      user: "system", 
-      password: "oracle123",
-      connectString: 'XE'
-    }, 
-    function(err, connection) {
-      if (err) {error = err; return;}
-      
-      connection.execute('select user from dual', [], function(err, result) {
-        if (err) {error = err; return;}
- 
-        user = result.rows[0][0];
-        error = null;
- 
-        connection.close(function(err) {
-          if (err) {console.log(err);}
-        });
-      })
+const app = express();
+const port = 3000;
+var password = 'oracle123';
+
+async function selectAllOperators(req, res) {
+  try {
+    connection = await oracledb.getConnection({
+      user: "system",
+      password: password,
+      connectString: "localhost:1521/xe"
+    });
+
+    console.log('connected to database');
+    // run query to get all employees
+    result = await connection.execute(`SELECT * FROM operator`);
+
+  } catch (err) {
+    //send error message
+    return res.send(err.message);
+  } finally {
+    if (connection) {
+      try {
+        // Always close connections
+        await connection.close();
+        console.log('close connection success');
+      } catch (err) {
+        console.error(err.message);
+      }
     }
-);
- 
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
- 
-  if (error === null) {
-    response.end('Connection test succeeded. You connected to Exadata Express as ' + user + '!');
-  } else if (error instanceof Error) {
-    response.write('Connection test failed. Check the settings and redeploy app!\n');
-    response.end(error.message);
-  } else {
-    response.end('Connection test pending. Refresh after a few seconds...');
+    if (result.rows.length == 0) {
+      //query return zero employees
+      return res.send('query send no rows');
+    } else {
+      //send all employees
+      return res.send(result.rows);
+    }
+
   }
-}).listen(8081);
+}
+
+//get /employess
+app.get('/operators', function (req, res) {
+  selectAllOperators(req, res);
+})
+
+async function selectEmployeesById(req, res, id) {
+  try {
+    connection = await oracledb.getConnection({
+      user: "system",
+      password: password,
+      connectString: "localhost:1521/xe"
+    });
+    // run query to get employee with employee_id
+    result = await connection.execute(`SELECT * FROM operator where id=:id`, [id]);
+
+  } catch (err) {
+    //send error message
+    return res.send(err.message);
+  } finally {
+    if (connection) {
+      try {
+        // Always close connections
+        await connection.close(); 
+      } catch (err) {
+        return console.error(err.message);
+      }
+    }
+    if (result.rows.length == 0) {
+      //query return zero employees
+      return res.send('query send no rows');
+    } else {
+      //send all employees
+      return res.send(result.rows);
+    }
+  }
+}
+
+//get /employee?id=<id employee>
+app.get('/operator', function (req, res) {
+  //get query param ?id
+  let id = req.query.id;
+  // id param if it is number
+  //if (isNaN(id)) {
+  //  res.send('Query param id is not number')
+  //  return
+  //}
+  selectEmployeesById(req, res, id);
+})
+
+app.listen(port, () => console.log("nodeOracleRestApi app listening on port %s!", port))

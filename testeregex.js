1661f33a9adeed260ddfe57dd771b8871cf44223
Ariane Barros <arianepaulabarros@gmail.com>
2021-11-05 11:55:52 -0300

    Organazing

--- testeregex.js
+++ testeregex.js
@@ -1,10 +1,11 @@
 //node testeregex.js
 
-var re = "[A-Z][A-Z][A-Z][0-9][A-Z0-9][0-9][0-9]";
-var capital_letters_array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var myArray = [];
-var cont = 0;
-while(cont < 20){
+function generate_license () {
+    var re = "[A-Z][A-Z][A-Z][0-9][A-Z0-9][0-9][0-9]";
+    var capital_letters_array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var myArray = [];
+    var cont = 0;
+
     var placa = "";
     
     for (i = 0; i < 3; i++){
@@ -19,17 +20,7 @@ while(cont < 20){
     
     myArray.push(placa)
     cont++;
+
+    console.log("Nova placa gerada com sucesso   " + myArray[0]);
+    return myArray[0];
 }
-console.log(myArray);
-//var myArray = ['AAA8A88', 'A909090', 'aaa8a88', 'aAA8A88', '12AAAAA', 'AAA9A99AA', 'AAA8888'];
-var validated = []
-myArray.forEach(function(item){
-    var value = item.toString().match(re);
-
-    if(value != null)
-        validated.push(value);
-})
-
-console.log(validated)
-
-    
